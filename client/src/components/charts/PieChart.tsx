48994d745597684bbdde61277f1a9054ee8ed3cc
13304 <duliba7@aol.com>
2023-08-06 16:04:18 -0400

    bs

--- client/src/components/charts/PieChart.tsx
+++ client/src/components/charts/PieChart.tsx
@@ -1,52 +1,12 @@
-import ReactApexChart from "react-apexcharts";
-import { Box, Typography, Stack } from "@pankod/refine-mui";
+import React, { useEffect, useState } from 'react';
 
-import { PieChartProps } from "interfaces/home";
+const PieChart = () => {
 
-const PieChart = ({ title, value, series, colors }: PieChartProps) => { //typescript syntax here and below with Box need to import via refine mui above
   return (
-    <Box 
-      id="chart"
-            flex={1}
-            display="flex"
-            bgcolor="#fcfcfc"
-            flexDirection="row"
-            justifyContent="space-between"
-            alignItems="center"
-            pl={3.5}
-            py={2}
-            gap={2}
-            borderRadius="15px"
-            minHeight="218px"
-            width="340px"
-        >
-            <Stack direction="column">
-                <Typography fontSize={14} color="#808191">
-                    {title}
-                </Typography>
-                <Typography
-                    fontSize={24}
-                    color="#11142d"
-                    fontWeight={700}
-                    mt={1}
-                >
-                    {value}
-                </Typography>
-            </Stack>
-
-            <ReactApexChart
-                options={{
-                    chart: { type: "donut" },
-                    colors,
-                    legend: { show: false },
-                    dataLabels: { enabled: false },
-                }}
-                series={series}
-                type="donut"
-                width="120px"
-            />
-    </Box>
-  )
-}
+    <div>
+     
+    </div>
+  );
+};
 
 export default PieChart;
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -18,36 +18,16 @@ const Home = () => {
         <Grid item xs={12} md={6}>
           <Grid container spacing={2}>
             <Grid item xs={6}>
-              <PieChart
-                title="Properties for Sale"
-                value={684}
-                series={[75, 25]}
-                colors={["#275be8", "#c4e8ef"]}
-              />
+              <PieChart />
             </Grid>
             <Grid item xs={6}>
-              <PieChart
-                title="Properties for Rent"
-                value={550}
-                series={[60, 40]}
-                colors={["#275be8", "#c4e8ef"]}
-              />
+              <PieChart />
             </Grid>
             <Grid item xs={6}>
-              <PieChart
-                title="Total customers"
-                value={5684}
-                series={[75, 25]}
-                colors={["#275be8", "#c4e8ef"]}
-              />
+              <PieChart />
             </Grid>
             <Grid item xs={6}>
-              <PieChart
-                title="Properties for Cities"
-                value={555}
-                series={[75, 25]}
-                colors={["#275be8", "#c4e8ef"]}
-              />
+              <PieChart />
             </Grid>
           </Grid>
         </Grid>
